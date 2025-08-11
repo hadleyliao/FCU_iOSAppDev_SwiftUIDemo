120e4806fb1fe5ea95e654699cab406a4f69f36c
Hadley Liao <154941260+hadleyliao@users.noreply.github.com>
2025-08-11 11:56:50 +0800

    8/11 update
    8/11 update

--- SwiftUIDemo/Demo/Observation/Counter.swift
+++ SwiftUIDemo/Demo/Observation/Counter.swift
@@ -5,6 +5,7 @@ struct Counter: View {
     @Binding var count: Int
 
     var body: some View {
+        let _ = Self._printChanges()
         Text("Counter: \(count)")
         Button("Reset") {
             count = 0
@@ -16,9 +17,16 @@ struct ModelCounter: View {
     var model: Model
 
     var body: some View {
+        @Bindable var model = model
+        
         Button("Set to 100") {
             model.count = 100
         }
+        
+        TextField(text: $model.name){
+            Text("Enter name")
+        }
+        .background(.red)
     }
 }
 
@@ -30,6 +38,8 @@ struct CounterView: View {
             model.count += 1
         }
 
+        Text("Name: \(model.name)")  // 這裡才能顯示 "計數"
+        
         Divider()
 
         Counter(count: $model.count)
@@ -40,9 +50,13 @@ struct CounterView: View {
     }
 }
 
+// 在 @Observable 右鍵 > 選 Expand Macro
+// 它會展開編譯器背後自動生成的程式碼，讓你看到 @Observable 實際被編譯成什麼樣的 Swift 程式碼。
+// 等於是你可以偷看編譯器的「黑箱」，把語法糖背後的原始代碼攤開來看。
 @Observable
 class Model {
-    var count: Int = 0
+    var count: Int = 50
+    var name: String = ""
 }
 
 #Preview {
--- SwiftUIDemo/Demo/Observation/CounterEnvironment.swift
+++ SwiftUIDemo/Demo/Observation/CounterEnvironment.swift
@@ -5,6 +5,21 @@ import Observation
 class CounterModel {
     var text: String = "Hello, World!"
     var count: Int = 0
+    
+    func test() {
+        var text: String = ""
+        if text == "" {
+            
+        }
+        
+        var text2: String? = nil    // Swift 的 nil 概念 = Java 的 null
+        text2 = "ABC"
+        if text2 == nil {
+        }
+        if let text2 = text2 {   // 可以簡化成 if let text2
+            
+        }
+    }
 }
 
 struct BindableCounter: View {
@@ -17,18 +32,19 @@ struct BindableCounter: View {
 }
 
 struct CounterEnvironment: View {
-    @Environment(CounterModel.self) private var model
+    @Environment(CounterModel.self) private var model:
+    CounterModel?
 
     var body: some View {
-        @Bindable var model = model
+//        @Bindable var model = model
+//
+//        TextField("", text: $model.text)
+//            .padding()
 
-        TextField("", text: $model.text)
-            .padding()
-
-        Text("Counter: \(model.count)")
+        Text("Counter: \(model?.count)")
 
         Button("Increment") {
-            model.count += 1
+            model?.count += 1
         }
     }
 }
